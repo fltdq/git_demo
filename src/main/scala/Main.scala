7c44ecee93f346c940437df7cb66b7be8d983ceb
chensihang <chensihang@xiaomi.com>
2022-07-29 09:39:15 +0800

    增加printAuthor方法

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,4 +4,8 @@ object Main {
     println("hell git")
     println("再输出一些信息")
   }
+
+  def printAuthor(name: String, sex: String, age: Int): Unit ={
+    println(s"name: $name, sex: $sex, age: $age")
+  }
 }
