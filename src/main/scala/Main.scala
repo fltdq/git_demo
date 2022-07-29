3d2fdafa2d8cff53870ea86061da9f53a2050656
chensihang <chensihang@xiaomi.com>
2022-07-29 09:32:09 +0800

    增加李白的信息

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,4 +4,15 @@ object Main {
     println("hell git")
     println("再输出一些信息")
   }
+
+  class people(name: String, sex:String, age:Int){
+
+    println(s"name: $name age: $age + sex: $sex")
+  }
+
+  var libai = new people("libai","男",48);
+  val lily = new people("lily","女",15)
+
+  println(libai)
+  println(lily)
 }
